96f0cb4846f584afe232f16fe48c46959f9142bf
codingscode <heronnestg@hotmail.com>
2023-05-29 22:22:32 -0300

    parte 21

--- parte_android/meuproj001/app/src/main/java/com/example/projeto01/MainActivity.java
+++ parte_android/meuproj001/app/src/main/java/com/example/projeto01/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.projeto01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        text = findViewById(R.id.texto);
+        
     }
 }
