07fe26bf498c082f06e29064e456c9ecdf0a2417
codingscode <heronnestg@hotmail.com>
2023-05-31 11:10:55 -0300

    parte 32

--- parte_android/meuproj001/app/src/main/java/com/example/projeto01/MainActivity.java
+++ parte_android/meuproj001/app/src/main/java/com/example/projeto01/MainActivity.java
@@ -2,19 +2,23 @@ package com.example.projeto01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView text;
+    TextView texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        //text = findViewById(R.id.texto);
+        texto = findViewById(R.id.textoid1);
+        
+        texto.setTextColor(Color.BLACK);
+        
         
     }
 }
