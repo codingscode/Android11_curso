9b3dc8c5332e2f4be8e300234214fdaeca7a596a
codingscode <heronnestg@hotmail.com>
2023-06-01 10:04:33 -0300

    parte 37

--- parte_android/meuproj001/app/src/main/java/com/example/projeto01/MainActivity.java
+++ parte_android/meuproj001/app/src/main/java/com/example/projeto01/MainActivity.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView texto;
+    Button botao;
+    
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,18 +21,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         
         texto = findViewById(R.id.textoid1);
+        botao = findViewById(R.id.botao2);
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               botao.setBackgroundColor(Color.BLACK);
+            }
+        });
         
         texto.setTextColor(Color.BLACK);
         
         texto.setText("outro texto setado");
+
         
-        texto.setOnClickListener(new View.OnClickListener() {
+
+
+
+        /*texto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                texto.setTextColor(Color.CYAN);
                texto.setBackgroundColor(Color.BLACK);
             }
-        });
+        });*/
         
         
     }
