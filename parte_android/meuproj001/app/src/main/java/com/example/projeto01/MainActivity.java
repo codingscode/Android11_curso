96d75f5e74be37cde215b22c7afd88d9d28a6bc9
codingscode <heronnestg@hotmail.com>
2023-06-02 09:41:38 -0300

    parte 45

--- parte_android/meuproj001/app/src/main/java/com/example/projeto01/MainActivity.java
+++ parte_android/meuproj001/app/src/main/java/com/example/projeto01/MainActivity.java
@@ -6,11 +6,15 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    EditText nome;
+    Button ok;
+    TextView resultado;
+    String nomeusuario;
     
 
     @Override
@@ -18,7 +22,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        nome = findViewById(R.id.textoeditavel);
+        ok = findViewById(R.id.botao_ok);
+        resultado = findViewById(R.id.textoresultado);
 
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               nomeusuario = nome.getText().toString();
+               resultado.setText(nomeusuario);
+            }
+        });
         
     }
 }
+
