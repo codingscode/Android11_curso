8a6d6c2698d18b760f7e2e6b919af1064ba1b57d
codingscode <heronnestg@hotmail.com>
2023-06-08 21:18:22 -0300

    parte 60

--- parte_android/meuproj001/app/src/main/java/com/example/projeto01/MainActivity.java
+++ parte_android/meuproj001/app/src/main/java/com/example/projeto01/MainActivity.java
@@ -1,24 +1,15 @@
 package com.example.projeto01;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button ok;
-    TextView resultado;
-    ImageView imagem;
-    LinearLayout layout;
+
+    TextView texto;
+
 
 
     @Override
@@ -26,40 +17,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ok = findViewById(R.id.botao_ok);
-        resultado = findViewById(R.id.textoresultado);
-        imagem = findViewById(R.id.imagem);
-        
-        layout = findViewById(R.id.layout_linear);
-        
-        
-        ok.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               mostrarMensagemDialog();
-            }
-        });
 
-    // testar também com LENGTH_SHORT
-    }
+        texto = findViewById(R.id.texto);
 
-    private void mostrarMensagemDialog() {
-       AlertDialog.Builder dialogAlerta = new AlertDialog.Builder(this);
-       dialogAlerta.setTitle("deletar")
-               .setMessage("Você quer deletar este texto?")
-               .setNegativeButton("não", new DialogInterface.OnClickListener() {
-                  @Override
-                  public void onClick(DialogInterface dialogInterface, int i) {
-                     dialogInterface.cancel();
-                  }
-               })
-               .setPositiveButton("sim", new DialogInterface.OnClickListener() {
-                   @Override
-                   public void onClick(DialogInterface dialogInterface, int i) {
-                      resultado.setText("");
-                   }
-               }).show();
-        dialogAlerta.create();
+        
+
+
+
     }
 }
 
---
+++ parte_android/meuproj001/app/src/main/java/com/example/projeto01/Splash_Activity.java
@@ -0,0 +1,17 @@
+package com.example.projeto01;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class Splash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+
+}
