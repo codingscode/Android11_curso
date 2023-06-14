9b58c23d1e297bd7c0cfb29948314bf6effe1502
codingscode <heronnestg@hotmail.com>
2023-06-13 21:27:41 -0300

    parte 67

--- parte_android/meuproj001/app/src/main/java/com/example/projeto01/GameActivity.java
+++ parte_android/meuproj001/app/src/main/java/com/example/projeto01/GameActivity.java
@@ -3,14 +3,33 @@ package com.example.projeto01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
+    private TextView ultimo, direito, dica;
+    private Button confirmar;
+    private EditText adivinhar;
+
+    boolean doisDig, tresDig, quatroDig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        ultimo = findViewById(R.id.textoultimo);
+        direito = findViewById(R.id.textodireito);
+        dica = findViewById(R.id.textodica);
+        confirmar = findViewById(R.id.botaoconfirmar);
+        adivinhar = findViewById(R.id.textoAdiv);
+
+        doisDig = getIntent().getBooleanExtra("dois", false);
+        tresDig = getIntent().getBooleanExtra("tres", false);
+        quatroDig = getIntent().getBooleanExtra("quatro", false);
+        
 
 
 
