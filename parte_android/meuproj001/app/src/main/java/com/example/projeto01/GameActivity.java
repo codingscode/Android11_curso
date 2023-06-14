56a500fda0fba4fcb20108170e5221262edb6c00
codingscode <heronnestg@hotmail.com>
2023-06-13 21:51:48 -0300

    parte 68

--- parte_android/meuproj001/app/src/main/java/com/example/projeto01/GameActivity.java
+++ parte_android/meuproj001/app/src/main/java/com/example/projeto01/GameActivity.java
@@ -3,17 +3,27 @@ package com.example.projeto01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
 
     private TextView ultimo, direito, dica;
     private Button confirmar;
-    private EditText adivinhar;
+    private EditText textoadivinhar;
 
     boolean doisDig, tresDig, quatroDig;
+    
+    Random r = new Random();
+    int aleatorio;
+    int restanteDireito = 10;
+    
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +34,60 @@ public class GameActivity extends AppCompatActivity {
         direito = findViewById(R.id.textodireito);
         dica = findViewById(R.id.textodica);
         confirmar = findViewById(R.id.botaoconfirmar);
-        adivinhar = findViewById(R.id.textoAdiv);
+        textoadivinhar = findViewById(R.id.textoAdiv);
 
         doisDig = getIntent().getBooleanExtra("dois", false);
         tresDig = getIntent().getBooleanExtra("tres", false);
         quatroDig = getIntent().getBooleanExtra("quatro", false);
+        
+        if (doisDig) {
+           aleatorio = r.nextInt(90) + 10;
+        }
+        if (tresDig) {
+           aleatorio = r.nextInt(900) + 100;
+        }
+        if (quatroDig) {
+           aleatorio = r.nextInt(9000) + 1000;
+        }
+
+        confirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               String adivinhar = textoadivinhar.getText().toString();
+               if (adivinhar.equals("")) {
+                  Toast.makeText(GameActivity.this, "tente uma adivinhação", Toast.LENGTH_LONG).show();
+                  
+               }
+               else {
+                  ultimo.setVisibility(View.VISIBLE);
+                  direito.setVisibility(View.VISIBLE);
+                  dica.setVisibility(View.VISIBLE);
+                  
+                  restanteDireito--;
+                  int adivUsuario = Integer.parseInt(adivinhar);
+                  
+                  ultimo.setText(adivinhar);
+                  direito.setText(restanteDireito);
+                  
+                  if (aleatorio == adivUsuario) {
+                  
+                  }
+                  if (aleatorio < adivUsuario) {
+                     dica.setText("Diminua o valor");
+                  }
+                  if (aleatorio > adivUsuario) {
+                     dica.setText("Aumente o valor");
+                  }
+                  if (restanteDireito == 0) {
+                  
+                  }
+                  
+                  textoadivinhar.setText("");
+                  
+               }
+            }
+        });
+
         
 
 
